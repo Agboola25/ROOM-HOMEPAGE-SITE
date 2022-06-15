a52de14c67e7e696a42579e78c8e92a72720ccc9
agnoStack <olorunlexe@gmail.com>
2022-06-15 15:35:16 +0100

    updated

--- Documents/JOECODE/JOEL CODING LESSONS/SPINNING WHEEL PROJECT/index.js
+++ Documents/JOECODE/JOEL CODING LESSONS/SPINNING WHEEL PROJECT/index.js
@@ -1,47 +1,119 @@
 const edit = document.getElementById("edit"); //selector for edit property
 const result = document.getElementById("result"); //selector for result property
 const hide = document.getElementById("hide"); //selector for hide property
+const circle = document.querySelector("#circle");
+const add = document.querySelector("#add");
+const inputsContainer = document.getElementById("inputs-container");
+const allInputs = [...document.querySelectorAll(".item")];
 
+// here ia a function i want to use to generage random colors for the backgroundColor
+const colors = ["red", "blue", "green", "yellow", "pink", "teal", "orange"];
+const selectRandomColor = (arr) => {
+  let random = Math.floor(Math.random() * arr.length);
+  return arr[random];
+};
+
+// here is the click function for the inputs to show and close on click
 edit.addEventListener("click", function () {
-  edit.style.color = "red";
-  result.style.color = "black";
-  hide.style.color = "black";
+  let height = inputsContainer.style.height;
+  if (height === "0px") {
+    inputsContainer.style.height = "auto";
+  } else {
+    inputsContainer.style.height = "0px";
+  }
 });
 
+// this is the shit you did
 result.addEventListener("click", function () {
   result.style.color = "red";
   edit.style.color = "black";
   hide.style.color = "black";
 });
 
-hide.addEventListener("click", function () {
-  hide.style.color = "red";
-  edit.style.color = "black";
-  result.style.color = "black";
-});
-
-const circle = document.querySelector("#circle");
-const add = document.querySelector("#add");
-
 let radi = [];
+let sections = [];
 
+// i made this stuff a function so we can call it anytime to give the radi angles
+const radLayout = (radi) => {
+  radi.forEach((item, index) => {
+    item.style.transform = `rotate(${
+      (index + 1) * (360 / radi.length) + 90
+    }deg)`;
+  });
+};
+
+// same here
+const sectionLayout = (sections) => {
+  sections.forEach((item, index) => {
+    const rot = () => {
+      if (sections.length % 4 === 0) {
+        return (index + 1) * (360 / sections.length) - 45;
+      } else if (sections.length % 2 === 0) {
+        return (index + 1) * (360 / sections.length);
+      } else {
+        return (index + 1) * (360 / sections.length) - 90;
+      }
+    };
+    // console.log(rot());
+    item.style.transform = `rotate(${rot()}deg)`;
+  });
+};
+
+// add new radius & section
 add.addEventListener("click", () => {
   const child = document.createElement("div");
   child.setAttribute("class", "radi");
-  const span = document.querySelector("#span");
-  span.innerHTML = radi.length + 1;
-  console.log(radi.length);
-
-  if (radi.length < 1) {
-    circle.appendChild(child.cloneNode(true));
-    circle.appendChild(child.cloneNode(true));
-    span.innerHTML = 2;
-  } else {
-    circle.appendChild(child);
-  }
-
-  radi = document.querySelectorAll(".radi");
-  radi.forEach((item, index) => {
-    item.style.transform = `rotate(${(index + 1) * (360 / radi.length)}deg)`;
+  const section = document.createElement("div");
+  section.setAttribute("class", "sections");
+  // adding unique id
+  sections.forEach((i, index) => {
+    i.setAttribute("id", `section${index}`);
   });
+
+  let inputValues = [];
+  allInputs.forEach((i) => {
+    if (i.value !== "") {
+      inputValues.push(i.value);
+    }
+  });
+
+  // let background = [...radi].map((i) => {
+  //   let color = selectRandomColor(colors);
+  //   let deg = ` ${360 / radi.length}`;
+  //   return `${color} ${deg}deg`;
+  // });
+  console.log(inputValues);
+
+  if (radi.length >= 7) {
+    return;
+  } else {
+    inputValues.forEach((i) => {
+      if (i) {
+        circle.appendChild(child.cloneNode(true));
+        circle.appendChild(section.cloneNode(true));
+
+        radi = document.querySelectorAll(".radi");
+        sections = document.querySelectorAll(".sections");
+
+        radLayout(radi);
+        sectionLayout(sections);
+        sections.forEach((i, index) => {
+          i.innerHTML = inputValues[index];
+        });
+      }
+    });
+    // circle.style.background = `conic-gradient(${background.join(",")})`;
+  }
+});
+
+hide.addEventListener("click", () => {
+  const allRadi = [...document.querySelectorAll(".radi")];
+  const allSections = [...document.querySelectorAll(".sections")];
+  // allRadi.splice(0, allRadi.length);
+  if (allRadi.length > 1) {
+    allRadi.forEach((i) => circle.removeChild(i));
+    allSections.forEach((i) => circle.removeChild(i));
+  } else {
+    return;
+  }
 });
--- Documents/JOECODE/JOEL CODING LESSONS/SPINNING WHEEL PROJECT/spin.js
+++ Documents/JOECODE/JOEL CODING LESSONS/SPINNING WHEEL PROJECT/spin.js
@@ -1,31 +1,87 @@
-$(window).load(function () {
-  let c = document.getElementById("canvas");
-  let ctx = c.getContext("2d");
-  let RADIUS = 300;
-  let num_sections = 6; //set this for number of divisions
+const add = document.getElementById("add");
+const inputsContainer = document.getElementById("inputs-container");
+const label = document.getElementById("newItem");
+const color = document.getElementById("newColor");
+const mainAdd = document.getElementById("mainAdd");
+const edit = document.getElementById("edit");
+const editContainer = document.querySelector(".edit-container");
+const editsInputsContainer = document.querySelector(".edit-inputs-container");
+const update = document.getElementById("update");
 
-  function drawCircle(x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.stroke();
-  }
-  function drawAngledLine(x, y, length, angle) {
-    const radians = (angle / 180) * Math.PI;
-    let endX = x + length * Math.cos(radians);
-    let endY = y - length * Math.sin(radians);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(endX, endY);
-    ctx.closePath();
-    ctx.stroke();
-     
+const updateLabel = [...document.querySelectorAll(".update-label")];
+const updateColor = [...document.querySelectorAll(".update-color")];
 
-  }
-  //draw circle outline
-  drawCircle(320, 320, RADIUS);
-  //loop the number of sections to draw each
-  for (i = 1; i <= num_sections; i++) {
-    drawAngledLine(320, 320, RADIUS, i * (360 / num_sections));
+let sectors = [];
+
+const ctx = document.querySelector("#wheel").getContext`2d`;
+const rad = ctx.canvas.width / 2;
+const PI = Math.PI;
+const TAU = 2 * PI;
+
+//* Draw sectors and  texts to canvas *//
+const drawSector = (sector, i, arr) => {
+  const arc = TAU / arr.length;
+  const ang = arc * i;
+  ctx.save();
+  // COLOR
+  ctx.beginPath();
+  ctx.fillStyle = sector.color;
+  ctx.moveTo(rad, rad);
+  ctx.arc(rad, rad, rad, ang, ang + arc);
+  ctx.lineTo(rad, rad);
+  ctx.fill();
+  // TEXT
+  ctx.translate(rad, rad);
+  ctx.rotate(ang + arc / 2);
+  ctx.textAlign = "right";
+  ctx.fillStyle = "#fff";
+  ctx.font = "bold 30px sans-serif";
+  ctx.fillText(sector.label, rad - 10, 10);
+  //
+  ctx.restore();
+};
+
+add.addEventListener("click", () => {
+  const height = inputsContainer.style.height;
+  if (height == "0px") {
+    inputsContainer.style.height = "auto";
+  } else {
+    inputsContainer.style.height = "0px";
   }
 });
-co
+
+mainAdd.addEventListener("click", () => {
+  if (label.value !== "") {
+    sectors.push({ label: label.value, color: color.value });
+    sectors.forEach(drawSector);
+    inputsContainer.style.height = "0px";
+  } else {
+    return;
+  }
+});
+
+let newSectors = [];
+
+edit.addEventListener("click", () => {
+  const display = editContainer.style.display;
+  if (display === "none") {
+    editContainer.style.display = "block";
+  } else {
+    editContainer.style.display = "none";
+  }
+
+  newSectors = sectors.map((item) => {
+    return `<div class="edit-input-item">
+      <input class='update-label' id="edit-label" type="text" value='${item.label}' />
+      <input class='update-color' id="edit-color" type="color" value='${item.color}' />
+    </div> `;
+  });
+
+  editsInputsContainer.innerHTML = newSectors;
+});
+
+update.addEventListener("click", () => {
+  //   console.log(newSectors);
+  console.log(updateLabel);
+  console.log(updateColor);
+});
