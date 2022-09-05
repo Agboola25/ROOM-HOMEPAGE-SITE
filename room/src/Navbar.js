5c0e62c7cac37a73e74e59f5ac8383cb0ca301e3
agnoStack <103324986+Agboola2004@users.noreply.github.com>
2022-09-05 19:46:28 +0100

    mobile view in development,navbar toggle remaining

--- room/src/Navbar.js
+++ room/src/Navbar.js
@@ -2,17 +2,21 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    
-      <nav className="Navbar">
-        <h1>room</h1>
-        <ul>
-          <li>home</li>
-          <li>shop</li>
-          <li>about</li>
-          <li>contact</li>
-        </ul>
-      </nav>
-   
+    <nav className="Navbar">
+      <div class="span">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <h1>room</h1>
+      <ul>
+        <li>home</li>
+        <li>shop</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+    </nav>
   );
 };
 
